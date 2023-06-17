968cf1c5e3e0c1d0c9e60c42d3f84d44ab09112e
Carola Stork <carola.stork@stud.tu-darmstadt.de>
2023-06-17 20:59:11 +0200

    added layer normalization

--- network_types.py
+++ network_types.py
@@ -5,10 +5,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class LinearActor(nn.Module):
-	def __init__(self, obs_space, action_space):
+	def __init__(self, obs_space, action_space, layer_normalization=False):
 		super(LinearActor, self).__init__()
 
 		self.architecture = (500, 400, 300)
+		self.layer_normalization = layer_normalization
 		
 		self.linear_layers = nn.ModuleList()
 		self.linear_layers.append(nn.Linear(obs_space[1].shape[0]*obs_space[1].shape[1] + obs_space[0].shape[0], self.architecture[0]))
@@ -16,7 +17,10 @@ class LinearActor(nn.Module):
 			self.linear_layers.append(nn.Linear(self.architecture[i], dim))
 		self.linear_layers.append(nn.Linear(300, action_space.shape[0]))
 		
-		# TODO check for norm stuff from yannik!
+		if self.layer_normalization:
+			self.lnorm1 = nn.LayerNorm(obs_space[1].shape[0]*obs_space[1].shape[1]+obs_space[0].shape[0])
+			self.lnorms = [nn.LayerNorm(dim) for dim in self.architecture]
+			self.lnorms = nn.ModuleList(self.lnorms)
 
 		
 
@@ -25,19 +29,23 @@ class LinearActor(nn.Module):
 
 		a = torch.cat([a, state_jug], 1)
 
-		for linear_layer in self.linear_layers[:-1]:
-			a = F.relu(linear_layer(a))
-		a = torch.tanh(self.linear_layers[-1](a))
+		if self.layer_normalization:
+			a = self.lnorm1(a)
 
-		#TODO check for norm stuff from yannik!
+		for i, linear_layer in enumerate(self.linear_layers[:-1]):
+			a = F.relu(linear_layer(a))
+			if self.layer_normalization:
+				a = self.lnorms[i](a)
+		a = torch.tanh(self.linear_layers[-1](a))
 
 		return a
 
 class LinearQNetwork(nn.Module):
-	def __init__(self, obs_space, action_space):
+	def __init__(self, obs_space, action_space, layer_normalization=False):
 		super(LinearQNetwork, self).__init__()
 
 		self.architecture = (500, 400, 300)
+		self.layer_normalization = layer_normalization
 		
 		self.linear_layers = nn.ModuleList()
 		self.linear_layers.append(nn.Linear(obs_space[1].shape[0]*obs_space[1].shape[1] + obs_space[0].shape[0] + action_space.shape[0], self.architecture[0]))
@@ -45,7 +53,10 @@ class LinearQNetwork(nn.Module):
 			self.linear_layers.append(nn.Linear(self.architecture[i], dim))
 		self.linear_layers.append(nn.Linear(300, action_space.shape[0]))
 
-		# TODO check for norm stuff
+		if self.layer_normalization:
+			self.lnorm1 = nn.LayerNorm(obs_space[1].shape[0]*obs_space[1].shape[1]+obs_space[0].shape[0]+action_space.shape[0])
+			self.lnorms = [nn.LayerNorm(dim) for dim in self.architecture]
+			self.lnorms = nn.ModuleList(self.lnorms)
 
 	def forward(self, state_jug, state_particles, action):
 		q = torch.flatten(state_particles, 1)
@@ -53,20 +64,24 @@ class LinearQNetwork(nn.Module):
 
 		q = torch.cat([q, state_jug, action], 1)
 
-		for linear_layer in self.linear_layers[:-1]:
+		if self.layer_normalization:
+			q = self.lnorm1(q)
+
+		for i, linear_layer in enumerate(self.linear_layers[:-1]):
 			q = F.relu(linear_layer(q))
+			if self.layer_normalization:
+				q = self.lnorms[i](q)
 
 		q = torch.tanh(self.linear_layers[-1](q))
-
-		#TODO check for norm stuff from yannik!
 
 		return q
 
 class Convolution_Actor(nn.Module):
-	def __init__(self, obs_space, action_space):
+	def __init__(self, obs_space, action_space, layer_normalization=False):
 		super(Convolution_Actor, self).__init__()
 
 		self.architecture = (500, 400, 300)
+		self.layer_normalization = layer_normalization
 
 		self.conv = nn.Conv2d(1, 1, kernel_size=(1, obs_space[1].shape[1]+obs_space[0].shape[0]), stride=1)
 		
@@ -76,7 +91,10 @@ class Convolution_Actor(nn.Module):
 			self.linear_layers.append(nn.Linear(self.architecture[i], dim))
 		self.linear_layers.append(nn.Linear(300, action_space.shape[0]))
 		
-		# TODO check for norm stuff from yannik!
+		if self.layer_normalization:
+			self.lnorm1 = nn.LayerNorm(obs_space[1].shape[0]+obs_space[0].shape[0])
+			self.lnorms = [nn.LayerNorm(dim) for dim in self.architecture]
+			self.lnorms = nn.ModuleList(self.lnorms)
 
 		
 
@@ -90,19 +108,23 @@ class Convolution_Actor(nn.Module):
 
 		a = torch.cat([a, state_jug], 1)
 
-		for linear_layer in self.linear_layers[:-1]:
-			a = F.relu(linear_layer(a))
-		a = torch.tanh(self.linear_layers[-1](a))
+		if self.layer_normalization:
+			a = self.lnorm1(a)
 
-		#TODO check for norm stuff from yannik!
+		for i, linear_layer in enumerate(self.linear_layers[:-1]):
+			a = F.relu(linear_layer(a))
+			if self.layer_normalization:
+				a = self.lnorms[i](a)
+		a = torch.tanh(self.linear_layers[-1](a))
 
 		return a
 
 class ConvolutionQNetwork(nn.Module):
-	def __init__(self, obs_space, action_space):
+	def __init__(self, obs_space, action_space, layer_normalization=False):
 		super(ConvolutionQNetwork, self).__init__()
 
 		self.architecture = (500, 400, 300)
+		self.layer_normalization = layer_normalization
 
 		self.conv = nn.Conv2d(1, 1, kernel_size=(1, obs_space[1].shape[1]+obs_space[0].shape[0]), stride=1)
 		
@@ -112,7 +134,10 @@ class ConvolutionQNetwork(nn.Module):
 			self.linear_layers.append(nn.Linear(self.architecture[i], dim))
 		self.linear_layers.append(nn.Linear(300, action_space.shape[0]))
 
-		# TODO check for norm stuff
+		if self.layer_normalization:
+			self.lnorm1 = nn.LayerNorm(obs_space[1].shape[0]+obs_space[0].shape[0]+action_space.shape[0])
+			self.lnorms = [nn.LayerNorm(dim) for dim in self.architecture]
+			self.lnorms = nn.ModuleList(self.lnorms)
 
 	def forward(self, state_jug, state_particles, action):
 		t = state_jug.unsqueeze(1).expand(-1, 350, -1)
@@ -124,20 +149,23 @@ class ConvolutionQNetwork(nn.Module):
 
 		q = torch.cat([q, state_jug, action], 1)
 
-		for linear_layer in self.linear_layers[:-1]:
-			q = F.relu(linear_layer(q))
-		q = torch.tanh(self.linear_layers[-1](q))
+		if self.layer_normalization:
+			q = self.lnorm1(q)
 
-		#TODO check for norm stuff from yannik!
+		for i, linear_layer in enumerate(self.linear_layers[:-1]):
+			q = F.relu(linear_layer(q))
+			if self.layer_normalization:
+				q = self.lnorms[i](q)
+		q = torch.tanh(self.linear_layers[-1](q))
 
 		return q
 
 class Critic(nn.Module):
-	def __init__(self, obs_space, action_space, q_network_type):
+	def __init__(self, obs_space, action_space, q_network_type, layer_normalization=False):
 		super(Critic, self).__init__()
 
-		self.q1 = q_network_type(obs_space, action_space)
-		self.q2 = q_network_type(obs_space, action_space)
+		self.q1 = q_network_type(obs_space, action_space, layer_normalization)
+		self.q2 = q_network_type(obs_space, action_space, layer_normalization)
 
 	def forward(self, state_jug, state_particles, action):
 		return self.q1(state_jug, state_particles, action), self.q2(state_jug, state_particles, action)
